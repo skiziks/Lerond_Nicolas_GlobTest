7803e4629a09a7d6760866ee3996f56fb2abf524
skiziks <82734149+skiziks@users.noreply.github.com>
2024-05-17 13:10:37 +0200

    update

--- Question_2.php
+++ Question_2.php
@@ -4,9 +4,8 @@ function foo(array $arrays)
     // Initialisation du tableau finale qui comprendra les sous-tableaux fusionnés
     $result = [];
 
-    // Boucle qui parcourt les sous-tableaux donnés en entrée
+    // Boucle pour créer le tableau final qui comprendra les sous-tableaux fusionnés ou non
     foreach ($arrays as $subArray) {
-        // Variable qui définit si le sous-tableau en question doit être fusionné avec un groupe existant ou être ajouté au tableau finale comme un nouveau groupe
         $merged = false;
 
         // Parcours du tableau finale pour vérifier si le sous-tableau en question doit être fusionné avec un groupe existant
@@ -14,9 +13,9 @@ function foo(array $arrays)
             // Vérification de chevauchement avec les éléments existants à savoir si le subArray donc le tableau qui se fait analysé chevauche le groupe donc le tableau qui est déjà dans le tableau final
             if ($subArray[0] <= $group[1] && $subArray[1] >= $group[0]) {
                 // Fusion des suites en mettant à jour les bornes
-                $group[0] = min($group[0], $subArray[0]); // On prend le minimum des deux bornes
-                $group[1] = max($group[1], $subArray[1]); // On prend le maximum des deux bornes
-                $merged = true; // On met la variable merged à true pour dire que le sous-tableau a été fusionné
+                $group[0] = min($group[0], $subArray[0]);
+                $group[1] = max($group[1], $subArray[1]);
+                $merged = true; // = sous-tableau a été fusionné
                 break;
             }
         }
@@ -27,7 +26,28 @@ function foo(array $arrays)
         }
     }
 
-    // Formatage et affichage du résultat avec map et implode pour avoir le rendu souhaité : [[valeur, valeur], [valeur, valeur]]
+
+
+    // Trier les tableaux 
+
+    if (!function_exists('compareArrays')) {
+        // Définir la fonction compareArrays
+        function compareArrays($a, $b)
+        {
+            $sumA = array_sum($a);
+            $sumB = array_sum($b);
+            if ($sumA == $sumB) {
+                return 0;
+            }
+            return ($sumA < $sumB) ? -1 : 1;
+        }
+    }
+
+    // Tri du tableau $result en utilisant la fonction de comparaison personnalisée
+    usort($result, 'compareArrays');
+
+
+    // Formatage et affichage du résultat avec map et implode pour avoir le rendu souhaité
     $formattedResult = array_map(function ($group) {
         return '[' . implode(', ', $group) . ']';
     }, $result);
@@ -38,15 +58,13 @@ function foo(array $arrays)
 
 
 // Exemple d'utilisation avec les données de l'énoncé
-foo([[0, 3], [6, 10]]);
-foo([[0, 5], [3, 10]]);
-foo([[0, 5], [2, 4]]);
-foo([[7, 8], [3, 6], [2, 4]]);
-foo([[3, 6], [3, 4], [15, 20], [16, 17], [1, 4], [6, 10], [3, 6]]);
+foo([[0, 3], [6, 10]]); // [[0, 3], [6, 10]]
+foo([[0, 5], [3, 10]]); // [[0, 10]]
+foo([[0, 5], [2, 4]]); // [[0, 5]]
+foo([[7, 8], [3, 6], [2, 4]]); // [[2, 6], [7, 8]]
+foo([[3, 6], [3, 4], [15, 20], [16, 17], [1, 4], [6, 10], [3, 6]]); // [[1, 10], [15, 20]]
 
 // Exemple d'utilisation avec des données supplémentaires
-foo([[0, 3], [6, 10], [11, 15]]);
-foo([[0, 5], [3, 10], [11, 15]]);
-foo([[0, 5], [2, 4], [6, 10]]);
-foo([[7, 8], [3, 6], [2, 4], [9, 12]]);
-foo([[3, 6], [3, 4], [15, 20], [16, 17], [1, 4], [6, 10], [3, 6], [21, 25]]);
+foo([[0, 3], [6, 10], [11, 15]]); // [[0, 3], [6, 10], [11, 15]]
+foo([[7, 8], [3, 6], [2, 4], [9, 12]]); // [[2, 6], [7, 8], [9, 12]]
+foo([[3, 6], [3, 4], [15, 20], [16, 17], [1, 4], [6, 10], [3, 6], [21, 25]]); // [[1, 10], [15, 20], [21, 25]]
